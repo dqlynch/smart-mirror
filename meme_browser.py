2e79abc67414de6e47e3d9773e3f6c80f57ce210
Dylan Lynch <dqlynch@umich.edu>
2017-09-23 03:11:39 -0400

    Add reddit bot meme browser
    Also add .gitignore

---
+++ meme_browser.py
@@ -0,0 +1,21 @@
+import praw
+import time
+
+def gen_urls_from_sub(sub):
+    """Initializes a reddit instance and browses the given sub for urls"""
+    reddit = praw.Reddit('meme_browser')
+    subreddit = reddit.subreddit(sub)
+
+    for submission in subreddit.hot():
+        # Skip selfposts
+        if submission.url[-1] == '/':
+            continue
+        yield submission.url
+
+
+if __name__ == '__main__':
+    print('Testing meme_browser. Generating 1 url per second.')
+    input('Press enter to continue')
+    for url in gen_urls_from_sub('me_irl'):
+        print(url)
+        time.sleep(1)
