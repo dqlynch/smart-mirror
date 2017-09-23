830f5ea7b200daf032b32d56f2a43e08d3908334
izakfr <izakfr@umich.edu>
2017-09-23 19:28:46 -0400

    Add date and fixed image sizing

--- cgi-bin/ui.py
+++ cgi-bin/ui.py
@@ -10,46 +10,50 @@ import time
 class ui(Tk):
     def __init__(self):
         Tk.__init__(self)
-        url = ''
-        for x in meme_browser.gen_urls_from_sub('me_irl'):
-            url = x
-            break;
-        urllib.request.urlretrieve(str(url), 'meme.jpg')
-
         # Set the monitor to fullscreen
-        s_width = self.winfo_screenwidth()
-        s_height = self.winfo_screenheight()
-        geo_string = str(s_width) + "x" + str(s_height)
+        self.s_width = self.winfo_screenwidth()
+        self.s_height = self.winfo_screenheight()
+        geo_string = str(self.s_width) + "x" + str(self.s_height)
         self.geometry(geo_string)
 
         # Create instance of weatherData to use
         wd = weather.weatherData()
 
-        text_frame = Frame(self, bg="#000000")
-        text_frame.pack(fill=X)
+        # Stores the first line of text
+        text_frame1 = Frame(self, bg="#000000")
+        text_frame1.pack(fill=X)
 
-        seperator = Frame(self, bg="#000000")
-        seperator.pack(fill=BOTH, expand=1)
+        # Stores the second line of text
+        text_frame2 = Frame(self, bg="#000000")
+        text_frame2.pack(fill=X)
 
-        # Get the image and resize it
-        image = Image.open('meme.jpg')
-        image = image.resize((int(s_width/2 - 50), int(s_height/2 - 50)), Image.ANTIALIAS)
-        image.save('meme.png', 'PNG')
-        photo = PhotoImage(file="meme.png")
-        self.my_image = Label(seperator, image=photo, anchor=W, bg="#000000")
-        self.my_image.image = photo
-        self.my_image.pack(fill=BOTH, side=BOTTOM)
+        # This is for the meme, if the user wants to show it
+        self.seperator = Frame(self, bg="#000000")
+        self.seperator.pack(fill=BOTH, expand=1)
 
         # Display the time
-        self.disp_time = Message(text_frame, text=time.strftime("%H:%M"),
+        self.disp_time = Message(text_frame1, text=time.strftime("%H:%M"),
                                  bg="#000000", fg="#FFF")
-        self.disp_time.config(width=250, font=('times', 36))
+        self.disp_time.config(width=250, font=('arial', 36))
         self.disp_time.pack(side=LEFT, fill=X)
 
         # Set the message to be the current temperature
-        msg = Message(text_frame, text=wd.data[0], bg="#000000", fg="#FFF")
-        msg.config(width=250, font=('times', 36))
-        msg.pack(side=RIGHT, fill=X)
+        self.temp = Message(text_frame1, text=str(wd.data[0]) + "°", bg="#000000", fg="#FFF")
+        self.temp.config(width=250, font=('arial', 36))
+        self.temp.pack(side=RIGHT, fill=X)
+
+        # Set up the date Message
+        t = time.localtime()
+        date_data = time.asctime(t)
+        self.date = Message(text_frame2, text=date_data[0:11], bg="#000000", fg="#FFF")
+        self.date.config(width=250, font=('arial', 12), padx=12)
+        self.date.pack(side=LEFT, fill=X)
+
+        # Setup the weather max and main
+        self.weather_highlow = Message(text_frame2, text=str(wd.data[1]) + "/" + str(wd.data[0]),
+                                       bg="#000000", fg="#FFF")
+        self.weather_highlow.config(width=250, font=('arial'))
+
 
     def update_clock(self):
         am_or_pm = "am"
@@ -61,7 +65,37 @@ class ui(Tk):
         self.disp_time.configure(text=now)
         self.after(1000, self.update_clock)
 
+    def display_meme(self):
+        url = ''
+        for x in meme_browser.gen_urls_from_sub('me_irl'):
+            url = x
+            break;
+        urllib.request.urlretrieve(str(url), 'meme.jpg')
+
+        # Get the image and resize it
+        image = Image.open('meme.jpg')
+        im_width, im_height = image.size
+        resize_factor = 0
+        if(im_height > 600):
+            resize_factor = im_height/600
+            new_width = int(im_width/resize_factor)
+            new_height = int(im_height/resize_factor)
+            image = image.resize((new_width, new_height), Image.ANTIALIAS)
+        elif(im_width > 600):
+            resize_factor = im_width/600
+            new_width = int(im_width/resize_factor)
+            new_height = int(im_height/resize_factor)
+            image = image.resize((new_width, new_height), Image.ANTIALIAS)
+
+        image.save('meme.png', 'PNG')
+        photo = PhotoImage(file="meme.png")
+        self.my_image = Label(self.seperator, image=photo, anchor=W, bg="#000000")
+        self.my_image.image = photo
+        self.my_image.pack(fill=BOTH, side=BOTTOM)
+
+
 if __name__ == "__main__":
     root = ui()
     root.update_clock()
+    root.display_meme()
     root.mainloop()
--- cgi-bin/weather.py
+++ cgi-bin/weather.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 import requests
 
-def format_float(in_val):
-    return float("{:.2f}".format(in_val))
+def format_int(in_val):
+    return int(in_val)
 
 class weatherData:
     def __init__(self):
@@ -13,6 +13,6 @@ class weatherData:
         req = requests.get('http://api.openweathermap.org/data/2.5/weather?q=AnnArbor,uk&appid=0fc3d15d1af0e86484b6bca31c5c2504')
         data = req.json()
         # Change the temperature from Kelvin to Farenheit
-        self.data.insert(0, format_float(data['main']['temp']*9/5 - 459.67))
-        self.data.insert(1, format_float(data['main']['temp_max']*9/5 - 459.67))
-        self.data.insert(2, format_float(data['main']['temp_min']*9/5 - 459.67))
+        self.data.insert(0, format_int(data['main']['temp']*9/5 - 459.67))
+        self.data.insert(1, format_int(data['main']['temp_max']*9/5 - 459.67))
+        self.data.insert(2, format_int(data['main']['temp_min']*9/5 - 459.67))
