53f04d38355a4c2929dab1247a6d5d4ffd3b6aa7
izakfr <izakfr@umich.edu>
2017-09-23 14:47:34 -0400

    UI Update
    The UI is now a class and in main we create an instance of it.

--- ui.py
+++ ui.py
@@ -3,51 +3,39 @@ import weather
 import meme_browser
 import urllib.request
 from PIL import Image
+import time
 
-def ui():
-    root = Tk()
-    test = 0
-    url = ''
-    for x in meme_browser.gen_urls_from_sub('me_irl'):
-        ++test
-        url = x
-        if test == 3:
-            break
-    urllib.request.urlretrieve(str(url), 'meme.jpg')
+class ui(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        url = ''
+        for x in meme_browser.gen_urls_from_sub('me_irl'):
+            url = x
+            urllib.request.urlretrieve(str(url), 'meme.jpg')
 
+        # Create instance of weatherData to use
+        wd = weather.weatherData()
 
-    # Setup the window size
-    geo_string = str(root.winfo_screenwidth()) + "x" + str(root.winfo_screenheight())
-    root.geometry(geo_string)
+        s_width = self.winfo_screenwidth()
+        s_height = self.winfo_screenheight()
 
-    # Create instance of weatherData to use
-    wd = weather.weatherData()
+        image = Image.open('meme.jpg')
+        image.save('meme.png', 'PNG')
+        photo = PhotoImage(file="meme.png")
+        my_image = Label(self, image=photo)
+        my_image.image = photo
+        my_image.grid(row=4, column=0)
 
-    # Add a seperator for the UI
-    seperator = Frame(root, height=200, width=200)
-    seperator.pack(side=LEFT)
+        disp_time = Message(self, text=time.strftime("%H:%M:%S"))
+        disp_time.config(width=250, font=('times', 16))
+        disp_time.grid(row=0, column=0)
 
-    # Add a seperator for the meme
-    meme_seperator = Frame(root, width=300)
-    meme_seperator.pack(fill=Y, side=RIGHT)
+        # Set the message to be the current temperature
+        msg = Message(self, text=wd.data[0])
+        msg.config(width=250, font=('times', 16), anchor=N)
+        msg.grid(row=0, column=1)
 
-    # Insert the meme of the day
-    image = Image.open('meme.jpg')
-    image.save('meme.png', 'PNG')
-    photo = PhotoImage(file="meme.png")
-    my_image = Label(meme_seperator, image=photo)
-    my_image.pack()
-
-    # Set the message to be the current temperature
-    title = Message(seperator, text='Current Temperature: ')
-    title.config(width=250, font=('times', 16))
-    title.pack(side=LEFT)
-
-    msg = Message(seperator, text=wd.data[0])
-    msg.config(width=250, font=('times', 16))
-    msg.pack(side=LEFT)
-
-    root.mainloop()
 
 if __name__ == "__main__":
-    ui()
+    root = ui()
+    root.mainloop()
